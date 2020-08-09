54db8ea6b3a7007680ced8365972b8419d41aac6
Sarah S <55965220+sarahsau@users.noreply.github.com>
2020-08-09 21:04:13 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,8 +1,41 @@
-# Purdue-Federal-You-Need-A-Budget-Converter-YNAB-
-This ruby script converts the transactions CSV file exported from Purdue Federal to a file able to be imported by YNAB
+# BCA to YNAB converter v1.0
+This ruby script converts the CSV file from Bank Central Asia (BCA) to a file able to be imported by You Need a Budget (YNAB) app.
+Created by sarahsau (itsme@sarahsausan.com) in 2020; Forked from wesmcouch (wesmcouch@gmail.com).
 
 
+<<<<<<< Updated upstream
 Run the script using: 
+=======
+## Download the BCA e-statement and put it in the script directory
+1. Login to klikBCA
+2. Go to Account Information > Account Statement
+3. Select the period of transactions that you want to import to YNAB   
+4. Click *Statement Download*, that will download the statement file in csv format.
+5. Put the file in the script directory
+
+Note: v1.0 assumes that all the downloaded transactions occured within the same year.
+If you want to import transactions that occured in different year (e.g. 20 Dec 2019 - 5 Jan 2020), split the statement download into multiple files according to the year.
+
+## Install Ruby
+If you haven't gotten Ruby installed, please do so.
+
+## In your console, navigate to the script directory , then run the following command
 ```
-ynabConverter.rb <exportedTransactions.csv> <FilenameToExport>
+ ynab_BCA_converter.rb <[statement_from_klikBCA].csv> <[converted_file_name].csv> <year_of_the_statement>
 ```
+[statement_from_klikBCA] is the file name of the statement you downloaded from klikBCA.
+Put it in the directory you're in for simpler access.
+
+[converted_file_name] is the file name for the conversion result.
+By default the file name is *converterResult*. It will be written in the directory you're in.
+
+<year_of_the_statement> is the year the transactions occured.
+
+Example:
+>>>>>>> Stashed changes
+```
+ynab_BCA_converter.rb JOHNDOE19062024.csv result.csv 2020
+```
+
+## Import the result to YNAB
+The resulting CSV will be generated in the directory. When you import to YNAB, select DD/MM/YYYY as the date format.
---
+++ ynab_BCA_converter.rb
@@ -0,0 +1,83 @@
+# BCA Transactions CSV to YNAB Converter.
+# Created by sarahsau (itsme@sarahsausan.com) in 2015; Forked from wesmcouch (wesmcouch@gmail.com).
+# run the script using ynabconverter.rb <exportedTransactions.csv> <FilenameToExport> <statement year>.
+# When you import to YNAB, select DD/MM/YYYY as the date format.
+
+require 'csv'
+file_name = ARGV[0]
+
+new_file_name = ARGV[1]
+if new_file_name.to_s == ''
+	new_file_name = "converted_result.csv"
+end
+
+statement_year = ARGV[2].to_s
+
+CSV.open(new_file_name, "wb") do |csv|
+	csv << ["Date", "Payee", "Memo", "Amount"]
+end
+
+CSV.foreach(file_name, skip_lines:/Balance/, skip_blanks: true) do |row|
+
+	# Ignore four beginning and ending rows and also pending transactions.
+	date = row[0].to_s
+	if date.include?('PEND') || date.include?('Name') || date.include?('Balance') || date.include?('Currency') || date.include?('Account') || date.include?('Credit') || date.include?('Debet')
+		next
+	end
+
+	# Format date in the first column, assuming that all transactions occur in the same year.
+
+	annoying_first_character = date.chr
+	date.delete!(annoying_first_character)
+	date_with_year = date.dup.insert(-1, '/' + statement_year)
+
+	unless date_with_year == "/#{statement_year}"
+		row[0] = date_with_year
+	end
+
+	# Delete 'branch' and 'running balance' columns.
+	 row.delete(row[2])
+	 row.delete(row[4])
+
+	# assign credit and debit to transactions
+	if row[3].to_s.include?('DB')
+		outflow = 0 - row[2].to_f
+		row[2].replace(outflow.to_s)
+	end
+
+	# Delete CR and DB.
+	row.delete(row[3])
+
+	# Move columns to the correct headers.
+	row[3] = row[2]
+	row[2] = row[1]
+	row[1] = ""
+
+	# Assign common payees.
+	memo_column = row[2].to_s
+	payee_column = row[1].to_s
+
+	if memo_column.include?('SHOPEEPAY')
+		payee_column << "ShopeePay"
+	elsif memo_column.include?('TOKOPEDIA')
+		payee_column << "Tokopedia"
+	elsif memo_column.include?('OVO')
+		payee_column << "OVO"
+	elsif memo_column.include?('SAYURBOX')
+		payee_column << "Sayurbox"
+	elsif memo_column.include?('GO-PAY')
+		payee_column << "Gopay"
+	elsif memo_column.include?('FAVE')
+		payee_column << "Fave"
+	elsif memo_column.include?('BUNGA') || memo_column.include?('PAJAK BUNGA') || memo_column.include?('BIAYA ADM')
+		payee_column << "BCA"
+	end
+
+# Insert results to the new file.
+	CSV.open(new_file_name, "a+") do |csv|
+		csv << row
+	end
+
+	# Show result in the console.
+	puts row.inspect
+end
