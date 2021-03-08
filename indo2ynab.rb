063d661cb6a23c21064efed64521ba023fa1e0e6
sarahsau <sausan.sarah@gmail.com>
2021-03-08 14:07:38 +0700

    correct indo2ynab.rb tabs

--- indo2ynab.rb
+++ indo2ynab.rb
@@ -7,16 +7,16 @@ file = ARGV[1]
 
 case bank
 when /bca/i
-	statement = BankCentralAsia.new(file, ARGV[2], ARGV[3])
+  statement = BankCentralAsia.new(file, ARGV[2], ARGV[3])
 
-	statement.output_file
-	statement.bca_processing
-	statement.assign_payee
+  statement.output_file
+  statement.bca_processing
+  statement.assign_payee
 
 when /bni/i
-	statement = BankNegaraIndonesia.new(file, ARGV[2])
+  statement = BankNegaraIndonesia.new(file, ARGV[2])
 
-	statement.output_file
-	statement.bni_processing
-	statement.assign_payee
+  statement.output_file
+  statement.bni_processing
+  statement.assign_payee
 end
